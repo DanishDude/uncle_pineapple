972aaef1126fa098df5c5b288cf949393ff1e7e9
DanishDude <christian.hinge@gmail.com>
2021-01-19 15:07:41 +0100

    recipe screen

--- client/src/Screens/Recipe.js
+++ client/src/Screens/Recipe.js
@@ -1,31 +1,76 @@
 import React, { useEffect } from 'react';
-import { ScrollView, StyleSheet, Text, View } from 'react-native';
-import { useDispatch, useSelector } from 'react-redux';
-import { getRecipe } from '../actions/recipes';
-import StandardCard from '../Components/StandardCard';
+import { SafeAreaView, ScrollView, StyleSheet, View } from 'react-native';
+import { Image, Text } from 'react-native-elements';
 
 const Recipe = (props) => {
     const { navigation, route } = props;
     const { recipe } = route.params;
+    const photo = require('../../assets/empty_plate.jpeg');
 
     useEffect(() => {
         navigation.setOptions({ title: recipe.title });
     }, [navigation, recipe]);
 
     return (
-        <View style={styles.container}>
-            <Text>The Recipe Screen - Yay!</Text>
-            <Text>{recipe.title}</Text>
-        </View>
+        <SafeAreaView style={styles.container}>
+            <ScrollView style={styles.scrollView}>
+                <Image source={recipe.photo ? { uri: recipe.photo } : photo} containerStyle={styles.photo} />
+                <View style={styles.textArea}>
+                    {recipe.ingredients ? (
+                        <View style={styles.subSection}>
+                            <Text h4 h4Style={styles.subTitle}>
+                                Ingredients
+                            </Text>
+                            {recipe.ingredients.map((ingredient) => {
+                                return (
+                                    <Text key={ingredient._id}>
+                                        - {`${ingredient.qty} ${ingredient.unit} ${ingredient.name}`}
+                                    </Text>
+                                );
+                            })}
+                        </View>
+                    ) : undefined}
+                    {recipe.method ? (
+                        <View style={styles.subSection}>
+                            <Text h4 h4Style={styles.subTitle}>
+                                Method
+                            </Text>
+                            <Text>{recipe.method}</Text>
+                        </View>
+                    ) : undefined}
+                </View>
+            </ScrollView>
+        </SafeAreaView>
     );
 };
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
+        minHeight: '100%',
         backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
+    },
+    scrollView: {
+        marginHorizontal: 5,
+        marginVertical: 10,
+    },
+    photo: {
+        resizeMode: 'contain',
+        aspectRatio: 1.5,
+        borderRadius: 5,
+    },
+    textArea: {
+        marginTop: 10,
+        marginLeft: 5,
+        marginBottom: 10,
+    },
+    subSection: {
+        marginBottom: 20,
+    },
+    subTitle: {
+        marginBottom: 5,
+    },
+    text: {
+        marginTop: 5,
     },
 });
 
